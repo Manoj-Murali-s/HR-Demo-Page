1d36e5a02624916a0b3faba76c7b5bcdb0e862bc
Manoj Murali S <113785117+Manoj-Murali-s@users.noreply.github.com>
2023-03-05 14:12:19 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,65 @@
+// for Header
+const hamburger = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('nav--visible');
+});
+
+// for Header
+
+
+
+// Slideshow Start
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000);
+}
+
+ // Slideshow End
+
+
+
+//  Brand Carousel Start
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
+
+//  Brand Carousel End
